6aa5efb0b31607ab6f00e2e26e49ec370666d73c
bgoonz <bryan.guner@gmail.com>
2021-10-12 10:08:34 +0000

    -update

--- build/precache-manifest.53db93c0f36831835e0998c46b3b242f.js
+++ build/precache-manifest.53db93c0f36831835e0998c46b3b242f.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e0023d164f2977fcb089c7e14d7a716",
-    "url": "/index.html"
+    revision: "5e0023d164f2977fcb089c7e14d7a716",
+    url: "/index.html",
   },
   {
-    "revision": "4ec77554fbe2ec7bdcbe",
-    "url": "/static/css/main.2dd2afdc.chunk.css"
+    revision: "4ec77554fbe2ec7bdcbe",
+    url: "/static/css/main.2dd2afdc.chunk.css",
   },
   {
-    "revision": "861022f439e7fe83f0ce",
-    "url": "/static/js/2.92f4f62a.chunk.js"
+    revision: "861022f439e7fe83f0ce",
+    url: "/static/js/2.92f4f62a.chunk.js",
   },
   {
-    "revision": "4ec77554fbe2ec7bdcbe",
-    "url": "/static/js/main.35b49368.chunk.js"
+    revision: "4ec77554fbe2ec7bdcbe",
+    url: "/static/js/main.35b49368.chunk.js",
   },
   {
-    "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    revision: "42ac5946195a7306e2a5",
+    url: "/static/js/runtime~main.a8a9905a.js",
   },
   {
-    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
-  }
+    revision: "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    url: "/static/media/logo.5d5d9eef.svg",
+  },
 ]);
--- build/static/js/main.35b49368.chunk.js
+++ build/static/js/main.35b49368.chunk.js
@@ -1,2 +1,113 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{38:function(e,n,t){e.exports=t.p+"static/media/logo.5d5d9eef.svg"},42:function(e,n,t){e.exports=t(96)},47:function(e,n,t){},48:function(e,n,t){},55:function(e,n){},57:function(e,n){},66:function(e,n){},68:function(e,n){},96:function(e,n,t){"use strict";t.r(n);var o=t(3),c=t.n(o),a=t(34),i=t.n(a),r=(t(47),t(35)),s=t(36),l=t(40),u=t(37),f=t(41),p=t(38),d=t.n(p),h=(t(48),t(39)),m=t.n(h),g=function(e){function n(){return Object(r.a)(this,n),Object(l.a)(this,Object(u.a)(n).apply(this,arguments))}return Object(f.a)(n,e),Object(s.a)(n,[{key:"onListenClick",value:function(){fetch("http://localhost:3001/api/speech-to-text/token").then(function(e){return e.text()}).then(function(e){var n=m()({access_token:e,objectMode:!0,extractResults:!0,format:!1});n.on("data",function(e){console.log(e)}),n.on("error",function(e){console.log(e)}),document.querySelector("#stop").onclick=n.stop.bind(n)}).catch(function(e){console.log(e)})}},{key:"render",value:function(){return c.a.createElement("div",{className:"App"},c.a.createElement("header",{className:"App-header"},c.a.createElement("img",{src:d.a,className:"App-logo",alt:"logo"}),c.a.createElement("button",{className:"btn",onClick:this.onListenClick},"Listen to Mic")))}}]),n}(o.Component);Boolean("localhost"===window.location.hostname||"[::1]"===window.location.hostname||window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));i.a.render(c.a.createElement(g,null),document.getElementById("root")),"serviceWorker"in navigator&&navigator.serviceWorker.ready.then(function(e){e.unregister()})}},[[42,1,2]]]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    38: function (e, n, t) {
+      e.exports = t.p + "static/media/logo.5d5d9eef.svg";
+    },
+    42: function (e, n, t) {
+      e.exports = t(96);
+    },
+    47: function (e, n, t) {},
+    48: function (e, n, t) {},
+    55: function (e, n) {},
+    57: function (e, n) {},
+    66: function (e, n) {},
+    68: function (e, n) {},
+    96: function (e, n, t) {
+      "use strict";
+      t.r(n);
+      var o = t(3),
+        c = t.n(o),
+        a = t(34),
+        i = t.n(a),
+        r = (t(47), t(35)),
+        s = t(36),
+        l = t(40),
+        u = t(37),
+        f = t(41),
+        p = t(38),
+        d = t.n(p),
+        h = (t(48), t(39)),
+        m = t.n(h),
+        g = (function (e) {
+          function n() {
+            return (
+              Object(r.a)(this, n),
+              Object(l.a)(this, Object(u.a)(n).apply(this, arguments))
+            );
+          }
+          return (
+            Object(f.a)(n, e),
+            Object(s.a)(n, [
+              {
+                key: "onListenClick",
+                value: function () {
+                  fetch("http://localhost:3001/api/speech-to-text/token")
+                    .then(function (e) {
+                      return e.text();
+                    })
+                    .then(function (e) {
+                      var n = m()({
+                        access_token: e,
+                        objectMode: !0,
+                        extractResults: !0,
+                        format: !1,
+                      });
+                      n.on("data", function (e) {
+                        console.log(e);
+                      }),
+                        n.on("error", function (e) {
+                          console.log(e);
+                        }),
+                        (document.querySelector("#stop").onclick =
+                          n.stop.bind(n));
+                    })
+                    .catch(function (e) {
+                      console.log(e);
+                    });
+                },
+              },
+              {
+                key: "render",
+                value: function () {
+                  return c.a.createElement(
+                    "div",
+                    { className: "App" },
+                    c.a.createElement(
+                      "header",
+                      { className: "App-header" },
+                      c.a.createElement("img", {
+                        src: d.a,
+                        className: "App-logo",
+                        alt: "logo",
+                      }),
+                      c.a.createElement(
+                        "button",
+                        { className: "btn", onClick: this.onListenClick },
+                        "Listen to Mic"
+                      )
+                    )
+                  );
+                },
+              },
+            ]),
+            n
+          );
+        })(o.Component);
+      Boolean(
+        "localhost" === window.location.hostname ||
+          "[::1]" === window.location.hostname ||
+          window.location.hostname.match(
+            /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+          )
+      );
+      i.a.render(c.a.createElement(g, null), document.getElementById("root")),
+        "serviceWorker" in navigator &&
+          navigator.serviceWorker.ready.then(function (e) {
+            e.unregister();
+          });
+    },
+  },
+  [[42, 1, 2]],
+]);
 //# sourceMappingURL=main.35b49368.chunk.js.map
