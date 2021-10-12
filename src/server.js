d19c03775d42c019b1fd90885e7d5c2e881584b7
bgoonz <bryan.guner@gmail.com>
2021-10-12 09:44:53 +0000

    -update

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const AuthorizationV1 = require('watson-developer-cloud/authorization/v1');
-const SpeechToTextV1 = require('watson-developer-cloud/speech-to-text/v1');
-const vcapServices = require('vcap_services');
-const cors = require('cors')
-const dotenv = require('dotenv')
+const AuthorizationV1 = require("watson-developer-cloud/authorization/v1");
+const SpeechToTextV1 = require("watson-developer-cloud/speech-to-text/v1");
+const vcapServices = require("vcap_services");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 // allows environment properties to be set in a file named .env
 dotenv.config();
@@ -12,25 +12,25 @@ dotenv.config();
 // on bluemix, enable rate-limiting and force https
 if (process.env.VCAP_SERVICES) {
   // enable rate-limiting
-  const RateLimit = require('express-rate-limit');
-  app.enable('trust proxy'); // required to work properly behind Bluemix's reverse proxy
+  const RateLimit = require("express-rate-limit");
+  app.enable("trust proxy"); // required to work properly behind Bluemix's reverse proxy
 
   const limiter = new RateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 100, // limit each IP to 100 requests per windowMs
-    delayMs: 0 // disable delaying - full speed until the max limit is reached
+    delayMs: 0, // disable delaying - full speed until the max limit is reached
   });
 
   //  apply to /api/*
-  app.use('/api/', limiter);
+  app.use("/api/", limiter);
 
   // force https - microphone access requires https in Chrome and possibly other browsers
   // (*.mybluemix.net domains all have built-in https support)
-  const secure = require('express-secure-only');
+  const secure = require("express-secure-only");
   app.use(secure());
 }
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + "/static"));
 app.use(cors());
 
 // token endpoints
@@ -41,16 +41,18 @@ var sttAuthService = new AuthorizationV1(
   Object.assign(
     {
       iam_apikey: process.env.SPEECH_TO_TEXT_IAM_APIKEY,
-      url: process.env.SPEECH_TO_TEXT_URL ? process.env.SPEECH_TO_TEXT_URL : SpeechToTextV1.URL
+      url: process.env.SPEECH_TO_TEXT_URL
+        ? process.env.SPEECH_TO_TEXT_URL
+        : SpeechToTextV1.URL,
     },
-    vcapServices.getCredentials('speech_to_text') // pulls credentials from environment in bluemix, otherwise returns {}
+    vcapServices.getCredentials("speech_to_text") // pulls credentials from environment in bluemix, otherwise returns {}
   )
 );
-app.use('/api/speech-to-text/token', function(req, res) {
-  sttAuthService.getToken(function(err, token) {
+app.use("/api/speech-to-text/token", function (req, res) {
+  sttAuthService.getToken(function (err, token) {
     if (err) {
-      console.log('Error retrieving token: ', err);
-      res.status(500).send('Error retrieving token');
+      console.log("Error retrieving token: ", err);
+      res.status(500).send("Error retrieving token");
       return;
     }
     res.send(token.token || token);
@@ -58,9 +60,12 @@ app.use('/api/speech-to-text/token', function(req, res) {
 });
 
 const port = process.env.PORT || process.env.VCAP_APP_PORT || 5000;
-app.listen(port, function() {
-  console.log(process.env.SPEECH_TO_TEXT_IAM_APIKEY)
-  console.log('Example IBM Watson Speech JS SDK client app & token server live at http://localhost:%s/', port);
+app.listen(port, function () {
+  console.log(process.env.SPEECH_TO_TEXT_IAM_APIKEY);
+  console.log(
+    "Example IBM Watson Speech JS SDK client app & token server live at http://localhost:%s/",
+    port
+  );
 });
 
 // Chrome requires https to access the user's microphone unless it's a localhost url so
@@ -68,15 +73,15 @@ app.listen(port, function() {
 // note: this is not suitable for production use
 // however bluemix automatically adds https support at https://<myapp>.mybluemix.net
 if (!process.env.VCAP_SERVICES) {
-  const fs = require('fs');
-  const https = require('https');
+  const fs = require("fs");
+  const https = require("https");
   const HTTPS_PORT = 5001;
 
   const options = {
-    key: fs.readFileSync(__dirname + '/keys/localhost.pem'),
-    cert: fs.readFileSync(__dirname + '/keys/localhost.cert')
+    key: fs.readFileSync(__dirname + "/keys/localhost.pem"),
+    cert: fs.readFileSync(__dirname + "/keys/localhost.cert"),
   };
-  https.createServer(options, app).listen(HTTPS_PORT, function() {
-    console.log('Secure server live at https://localhost:%s/', HTTPS_PORT);
+  https.createServer(options, app).listen(HTTPS_PORT, function () {
+    console.log("Secure server live at https://localhost:%s/", HTTPS_PORT);
   });
 }
